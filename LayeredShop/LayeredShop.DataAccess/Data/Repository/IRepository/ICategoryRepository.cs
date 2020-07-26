3446afd02ade272264d47b10a8103a961b002e36
farhadmpr <farhadmp@gmail.com>
2020-07-26 14:10:11 +0430

    Add ICategoryRepository

--- LayeredShop/LayeredShop.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ LayeredShop/LayeredShop.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using LayeredShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LayeredShop.DataAccess.Data.Repository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+        void Update(Category category);
     }
 }
