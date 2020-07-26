ad6f2683beb74159d4a6df5bee8639f606ca590a
farhadmpr <farhadmp@gmail.com>
2020-07-26 12:21:29 +0430

    Add Category model

---
+++ LayeredShop/LayeredShop.Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayeredShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
