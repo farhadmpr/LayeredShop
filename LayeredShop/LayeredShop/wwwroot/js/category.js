ecbd73dfabd83fe1067cd8e50a6946a84157d848
farhadmpr <farhadmp@gmail.com>
2020-08-02 13:43:22 +0430

    Add category.js

--- LayeredShop/LayeredShop/wwwroot/js/category.js
+++ LayeredShop/LayeredShop/wwwroot/js/category.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(function () {
+    loadDataTable()
+});
+
+function loadDataTable() {
+    let dataTable = $('#tblData').DataTable({
+        'ajax': {
+            'url': '/admin/category/GetAll',
+            'type': 'GET',
+            'datatype': ' json'
+        },
+        'columns': [
+            { 'data': 'name', 'width': '50%' },
+            { 'data': 'displayOrder', 'width': '20%' },
+            {
+                'data': 'id',
+                'render': function (data) {
+                    return `<div class="text-center">
+                                <a href="/admin/category/insert/${data}" class="btn btn-success">Edit</a> | 
+                                <a onclick=delete("/admin/category/delete/${data}") class="btn btn-danger">Delete</a>
+                            </div>`
+                }, 'width': '30%'
+            }
+
+        ],
+        "language": {
+            "emptyTable": "No records found"
+        },
+        "width": "100%"
+    })    
+}
