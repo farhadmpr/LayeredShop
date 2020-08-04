5da41216256abca691310fd155b6254c32e839b2
farhadmpr <farhadmp@gmail.com>
2020-08-04 12:09:33 +0430

    Add js script delete

--- LayeredShop/LayeredShop/wwwroot/js/category.js
+++ LayeredShop/LayeredShop/wwwroot/js/category.js
@@ -1,9 +1,11 @@
-﻿$(function () {
+﻿let dataTable
+
+$(function () {
     loadDataTable()
 });
 
 function loadDataTable() {
-    let dataTable = $('#tblData').DataTable({
+    dataTable = $('#tblData').DataTable({
         'ajax': {
             'url': '/admin/category/GetAll',
             'type': 'GET',
@@ -17,7 +19,7 @@ function loadDataTable() {
                 'render': function (data) {
                     return `<div class="text-center">
                                 <a href="/admin/category/insert/${data}" class="btn btn-success">Edit</a> | 
-                                <a onclick=delete("/admin/category/delete/${data}") class="btn btn-danger">Delete</a>
+                                <a onclick=del("/admin/category/delete/${data}") class="btn btn-danger">Delete</a>
                             </div>`
                 }, 'width': '30%'
             }
@@ -27,5 +29,45 @@ function loadDataTable() {
             "emptyTable": "No records found"
         },
         "width": "100%"
-    })    
+    })
 }
+
+function del(url) {
+    swal.fire({
+        title: 'Are you sure you want to delete?',
+        text: 'you will not be able to restore the content',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#ff0000',
+        confirmButtonText: 'Yes',
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                        }).fire({
+                            icon: 'success',
+                            title: data.message
+                        })
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                        }).fire({
+                            icon: 'warning',
+                            title: data.message
+                        })
+                    }
+                }
+            })
+        }
+    })
+}
+
