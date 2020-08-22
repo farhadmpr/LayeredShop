18d391c9e0ceaced1fe4ddb417545f891983a306
farhadmpr <farhadmp@gmail.com>
2020-08-22 11:00:33 +0430

    Add Service & Service ViewModel

---
+++ LayeredShop/LayeredShop.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayeredShop.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
---
+++ LayeredShop/LayeredShop/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LayeredShop.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LayeredShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            this._unitOfWork = unitOfWork;
+            this._hostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Json(
+                new
+                {
+                    data = _unitOfWork.Service.GetAll(includeProperties: "Category")
+                });
+        }
+    }
+}
